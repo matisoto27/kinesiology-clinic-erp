f717e5bda999e4ac6f71b1d3275b71292687cfad
matisoto27 <matiasfranciscosoto@hotmail.com>
2026-03-14 15:37:53 -0300

    fix: agregar atributo faltante nombreActividad en el modelo ActividadCombo

--- app/Models/ActividadCombo.php
+++ app/Models/ActividadCombo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -24,6 +25,13 @@ class ActividadCombo extends Model
     protected $casts = [
         'activo' => 'boolean'
     ];
+
+    protected function nombreActividad(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->actividad->nombre
+        );
+    }
 
     function actividad(): BelongsTo
     {
