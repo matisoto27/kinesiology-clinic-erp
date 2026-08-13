27b897d568fe18934d8ec470d7520f2a3248c020
matisoto27 <matiasfranciscosoto@hotmail.com>
2026-08-13 00:05:17 -0300

    refactor: absorber la baja de tipos_sintoma en el create de sintomas

--- database/migrations/2026_01_31_235236_crear_tabla_sintomas.php
+++ database/migrations/2026_01_31_235236_crear_tabla_sintomas.php
@@ -16,8 +16,6 @@ return new class extends Migration
 
             $table->string('nombre', length: 50)->unique();
             $table->boolean('activo')->default(true);
-
-            $table->foreignId('id_tipo')->constrained(table: 'tipos_sintoma');
         });
     }
 
